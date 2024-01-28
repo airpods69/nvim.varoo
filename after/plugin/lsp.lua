d9378e2ef285c0e0c69a1f3368a442f48aaf6c8b
Varenyam Bhardwaj <varenyambhardwaj123@gmail.com>
2024-01-28 11:34:49 +0530

    cmd tab works, and neovide zoom in zoom out

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -56,14 +56,13 @@ cmp.setup({
 })
 
 
-cmp.setup.cmdline(':', {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = 'path' }
-	}, {
-		{ name = 'cmdline' }
-	})
-})
+-- cmp.setup.cmdline(':', {
+-- 	mapping = cmp.mapping.preset.cmdline(),
+-- 	sources = cmp.config.sources({
+-- 		{ name = 'cmdline'},
+-- 		{ name = 'path' },
+-- 	})
+-- })
 
 
 
--- lua/varoo/remap.lua
+++ lua/varoo/remap.lua
@@ -9,3 +9,13 @@ vim.keymap.set("n", "<S-Tab>", vim.cmd.bprev)
 
 -- For closing current buffer
 vim.keymap.set("n", "<leader>x", vim.cmd.bd)
+
+-- Copy Paste
+vim.keymap.set({"x"}, "<C-c>", [["+y]])
+vim.keymap.set({"n","i"}, "<C-v>", [[<ESC>"+p <S-a>]])
+
+if vim.g.neovide == true then
+	vim.api.nvim_set_keymap("n", "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>", { silent = true })
+	vim.api.nvim_set_keymap("n", "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>", { silent = true })
+	vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
+end
