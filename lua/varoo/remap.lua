3b63289792f8fc3a95fe82e5ba009167de90ab01
Varenyam Bhardwaj <varenyambhardwaj123@gmail.com>
2024-01-31 22:59:42 +0530

    Added terminal stuff to make life easier

--- lua/varoo/remap.lua
+++ lua/varoo/remap.lua
@@ -10,6 +10,12 @@ vim.keymap.set("n", "<S-Tab>", vim.cmd.bprev)
 -- For closing current buffer
 vim.keymap.set("n", "<leader>x", vim.cmd.bd)
 
+-- Terminal
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+vim.keymap.set("t", "<C-w>", "<C-\\><C-n><C-w>")
+vim.keymap.set("n", "<C-w><C-\\>", ":vsplit<CR>:term<CR>")
+vim.keymap.set("n", "<C-w><C-->", ":split<CR>:term<CR>")
+
 -- Copy Paste
 vim.keymap.set({"x"}, "<C-c>", [["+y]])
 vim.keymap.set({"n","i"}, "<C-v>", [[<ESC>"+p <S-a>]])
