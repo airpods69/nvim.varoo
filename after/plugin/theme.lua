34b3c1a37e591105a33db0d2bace410dc1539114
airpods69 <airpods69@airpods69>
2024-08-08 11:47:49 +0530

    Added lsp signs, removed presence

---
+++ after/plugin/theme.lua
@@ -0,0 +1,26 @@
+vim.cmd("colorscheme nord")
+
+
+
+require("headlines").setup({
+    markdown = {
+        headline_highlights = {
+            "Headline1",
+            "Headline2",
+            "Headline3",
+            "Headline4",
+            "Headline5",
+            "Headline6",
+        },
+        codeblock_highlight = "CodeBlock",
+        dash_highlight = "Dash",
+        quote_highlight = "Quote",
+    },
+})
+
+require('lualine').setup {
+  options = {
+    theme = 'nord'
+  }
+}
+
