7022cc05fad1b7bc47aef010ba38322e3ad487c3
airpods69 <airpods69@airpods69>
2024-09-21 14:56:38 +0530

    Changed theme, tried adding jupyter

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,23 +1,23 @@
-vim.cmd("colorscheme nordfox")
+vim.cmd("colorscheme nord")
 
 require("transparent").setup({})
 
 
-require("headlines").setup({
-    markdown = {
-        headline_highlights = {
-            "Headline1",
-            "Headline2",
-            "Headline3",
-            "Headline4",
-            "Headline5",
-            "Headline6",
-        },
-        codeblock_highlight = "CodeBlock",
-        dash_highlight = "Dash",
-        quote_highlight = "Quote",
-    },
-})
+-- require("headlines").setup({
+--     markdown = {
+--         -- headline_highlights = {
+--         --     "Headline1",
+--         --     "Headline2",
+--         --     "Headline3",
+--         --     "Headline4",
+--         --     "Headline5",
+--         --     "Headline6",
+--         -- },
+--         codeblock_highlight = "CodeBlock",
+--         dash_highlight = "Dash",
+--         quote_highlight = "Quote",
+--     },
+-- })
 
 require('lualine').setup {
   options = {
